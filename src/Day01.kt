b21e81e42413101a2a65c9d692daf4538aa829ee
Tyler <kolatra03@gmail.com>
2022-05-15 23:52:52 -0600

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,18 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    var floor = 0
+    val input = readInput("day01")
+
+    for ((index, value) in input[0].split("").withIndex()) {
+        when (value) {
+            "(" -> floor++
+            ")" -> {
+                if (floor == 0) {
+                    println("entering basement at: ${index+1}")
+                }
+                floor--
+            }
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("final floor: $floor")
 }
